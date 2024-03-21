45e6850bcc9be38a50ad2c48e230e62f5d35e949
VarunShukla07 <94779584+VarunShukla07@users.noreply.github.com>
2024-03-21 14:21:00 +0530

    Time: 3 ms (79.91%) | Memory: 11.7 MB (15.82%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,3 +1,9 @@
+auto speedUp = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
 /**
  * Definition for singly-linked list.
  * struct ListNode {
