17725dd738143f39d05d079bc6ffa8249c4271e5
VarunShukla07 <94779584+VarunShukla07@users.noreply.github.com>
2024-03-24 10:32:45 +0530

    Time: 75 ms (74.38%) | Memory: 63.6 MB (77.16%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,22 +1,33 @@
-auto speedUp = []() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 0;
-}();
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int dup = 0;
-        unordered_map<int,int> mpp;
-        for(int i = 0 ; i < nums.size();i++){
-            mpp[nums[i]]++;
-        }
-        for(auto it : mpp){
-            if (it.second > 1 && it.second > dup){
-                dup = it.first;
+        //Better Approach
+        // int dup = 0;
+        // unordered_map<int,int> mpp;
+        // for(int i = 0 ; i < nums.size();i++){
+        //     mpp[nums[i]]++;
+        // }
+        // for(auto it : mpp){
+        //     if (it.second > 1 && it.second > dup){
+        //         dup = it.first;
+        //     }
+        // }
+        // return dup;
+        //Optimal Approach
+        int slow = nums[0];
+        int fast = nums[0];
+        while (true) {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        if (slow == fast) {
+                break;  
             }
         }
-        return dup;
+        fast = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return fast;
     }
 };
