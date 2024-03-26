ab6dad264d2a46b1e5f1b33ef14b54f6d8e048fe
VarunShukla07 <94779584+VarunShukla07@users.noreply.github.com>
2024-03-27 00:44:52 +0530

    Time: 27 ms (98.13%) | Memory: 43.1 MB (99.78%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -7,25 +7,43 @@ auto speedUp = []() {
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        unordered_map<int, int> mpp;
-        // int maxi = *max_element(nums.begin(),nums.end());
-        int maxi = 0;
-        for (int num : nums) {
-            if (num > 0) {
-                mpp[num]++;
-                maxi = max(maxi, num);
+        // unordered_map<int, int> mpp;
+        // // int maxi = *max_element(nums.begin(),nums.end());
+        // int maxi = 0;
+        // for (int num : nums) {
+        //     if (num > 0) {
+        //         mpp[num]++;
+        //         maxi = max(maxi, num);
+        //     }
+        // }
+        // for(int i = 0 ; i < nums.size();i++){
+        //     if(nums[i] > 0){
+        //         mpp[nums[i]]++;
+        //     }
+        // }
+        // for(int i = 1 ; i <= maxi ; i++){
+        //     if(mpp.find(i) == mpp.end()){
+        //         return i;
+        //     }
+        // }
+        // return maxi + 1;
+        int n = nums.size();
+        for(int& num : nums){
+            if( num < 1 || num > n){
+                num = n + 1;
+            } 
+        }
+        for(int i = 0 ; i < n ; i++){
+            int num = std::abs(nums[i]);
+            if(num <= n){
+                nums[num - 1] = -std::abs(nums[num-1]);
             }
         }
-        for(int i = 0 ; i < nums.size();i++){
+        for(int i = 0 ; i < n ; i++){
             if(nums[i] > 0){
-                mpp[nums[i]]++;
+                return i+1;
             }
         }
-        for(int i = 1 ; i <= maxi ; i++){
-            if(mpp.find(i) == mpp.end()){
-                return i;
-            }
-        }
-        return maxi + 1;
+        return n+1;
     }
 };
