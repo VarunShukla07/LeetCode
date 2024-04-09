6a23916f67fc67278f4c7dac28ccd8110743c2a8
VarunShukla07 <94779584+VarunShukla07@users.noreply.github.com>
2024-04-09 10:06:05 +0530

    Time: 11 ms (34.60%) | Memory: 22 MB (16.75%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,33 +11,48 @@
  */
 class Solution {
 public:
-    int height(TreeNode* root){
-        if(root == nullptr){
+    //Using level order traversal
+    // int height(TreeNode* root){
+    //     if(root == nullptr){
+    //         return 0;
+    //     }
+    //     return max(height(root->left),height(root->right))+1;
+    // }
+    // int diameterOfBinaryTree(TreeNode* root) {
+    //     if(root == nullptr){
+    //         return 0;
+    //     }
+    //     queue<TreeNode*> qt;
+    //     qt.push(root);
+    //     int maxdim = 0;
+    //     while(!qt.empty()){
+    //         TreeNode* temp = qt.front();
+    //         qt.pop();
+    //         int lh = height(temp->left);
+    //         int rh = height(temp->right);
+    //         int currdim = lh+rh;
+    //         maxdim = max(maxdim,currdim);
+    //         if(temp->left != nullptr){
+    //             qt.push(temp->left);
+    //         }
+    //         if(temp->right != nullptr){
+    //             qt.push(temp->right);
+    //         }
+    //     }
+    //     return maxdim;
+    // }
+    int height(TreeNode* node, int& diameter) {
+        if (!node) {
             return 0;
         }
-        return max(height(root->left),height(root->right))+1;
+        int lh = height(node->left, diameter);
+        int rh = height(node->right, diameter);
+        diameter = max(diameter, lh + rh);
+        return 1 + max(lh, rh);
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        if(root == nullptr){
-            return 0;
-        }
-        queue<TreeNode*> qt;
-        qt.push(root);
-        int maxdim = 0;
-        while(!qt.empty()){
-            TreeNode* temp = qt.front();
-            qt.pop();
-            int lh = height(temp->left);
-            int rh = height(temp->right);
-            int currdim = lh+rh;
-            maxdim = max(maxdim,currdim);
-            if(temp->left != nullptr){
-                qt.push(temp->left);
-            }
-            if(temp->right != nullptr){
-                qt.push(temp->right);
-            }
-        }
-        return maxdim;
+        int diameter = 0;
+        height(root, diameter);
+        return diameter;        
     }
 };
